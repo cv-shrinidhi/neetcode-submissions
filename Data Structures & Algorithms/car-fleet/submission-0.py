f5345db35c529e6f8b0bd38ec93ba5db0d18a783
cv-shrinidhi <c.v.shrinidhi@gmail.com>
2026-07-13 15:52:03 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p,s in zip(position, speed)]
+        stack = []
+        # this uses intersection logic
+        # if second last car time to reach target is less
+        # than last car then they become a fleet and 
+        # travel at last car's speed
+        for p,s in sorted(pair)[::-1]:
+            stack.append((target-p)/s)
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
